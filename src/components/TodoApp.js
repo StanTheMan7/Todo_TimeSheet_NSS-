11213aa8324c23d0f6e069f6eef58440798e6191
StanTheMan7 <sts.graur@gmail.com>
2021-07-06 11:13:45 +0200

    premiere commit rajout de css sur app.js creation de component todoapp rajout des inputs pour la date et temps

---
+++ src/components/TodoApp.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./todoapp.css"
+
+
+function TodoApp() {
+    return (
+        <div className='todo'>
+            <div>
+            <input type="text" name="text" id="text" placeholder="Add your name here"></input>
+            </div>
+            
+            <div>
+            <input type="text" name="text" id="text" placeholder="Add task Here "></input>
+            </div>
+
+            <div>
+            <input type="date" name="date" id="date"></input>
+            
+            </div>
+
+            <div>
+            <input type="time" name="time" id="time"></input>
+            
+            </div>
+
+            <div>
+            <input type="time" name="time" id="time"></input>
+            
+            </div>
+            
+            <div>
+            <button className="add-btn">Add Task</button>
+            </div>
+            
+        </div>
+    )
+}
+export default TodoApp;
