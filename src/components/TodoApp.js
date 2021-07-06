4e8bdccd0d480df87695357e3c1f692fc734ef1f
StanTheMan7 <sts.graur@gmail.com>
2021-07-06 16:36:05 +0200

    V1

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,37 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./todoapp.css"
 
 
 function TodoApp() {
+
+    const [task, setTask] = useState("")
+    const [task2, setTask2] = useState("")
+    const [task3, setTask3] = useState("")
+    const [task4, setTask4] = useState("")
+    const [task5, setTask5] = useState("")
+    const [taskList, setTaskList]= useState([])
+
+
+    const handleChange = (e) => {
+        setTask(e.target.value);
+    }
+    const handleChange2 = (e) => {
+        setTask2(e.target.value);
+    }
+    const handleChange3 = (e) => {
+        setTask3(e.target.value);
+    }
+    const handleChange4 = (e) => {
+        setTask4(e.target.value);
+    }
+    const handleChange5 = (e) => {
+        setTask5(e.target.value);
+    }
+    const addTask = () => {
+        if(task !== ""){
+            const taskDetails = {
+                id: Math.floor(Math.random()*1000),
+                value: task,
+                value2 : task2,
+                value3 : task3,
+                value4: task4,
+                value5: task5,
+                isCompleted: false
+            }
+            setTaskList([...taskList, taskDetails])
+        }
+    }
+    // const deleteTask = () =>{
+    //     let parent = document.querySelector('li');
+    //     parent.remove();
+    // }
+    console.log("taskList", taskList)
     return (
-        <div className='todo'>
-            <div>
-            <input type="text" name="text" id="text" placeholder="Add your name here"></input>
-            </div>
-            
-            <div>
-            <input type="text" name="text" id="text" placeholder="Add task Here "></input>
-            </div>
-
-            <div>
-            <input type="date" name="date" id="date"></input>
-            
-            </div>
-
-            <div>
-            <input type="time" name="time" id="time"></input>
-            
-            </div>
-
-            <div>
-            <input type="time" name="time" id="time"></input>
-            
-            </div>
-            
-            <div>
-            <button className="add-btn">Add Task</button>
-            </div>
-            
+        <div className='todo' >
+            <input type="text"  name="text" id="text" placeholder="Add your name here"onChange={(e) => handleChange(e)}></input>
+            <input type="text"  name="text" id="text" placeholder="Add task Here " onChange={(e) => handleChange2(e)}></input>
+            <input type="date"  name="date" id="date" onChange={(e) => handleChange3(e)}></input>
+            <input type="time"  name="time" id="time" onChange={(e) => handleChange4(e)}></input>
+            <input type="time" name="time" id="time" onChange={(e) => handleChange5(e)}></input>
+            <button className="add-btn" onClick={addTask}>Add Task</button>
+            {taskList !== [] ?   
+            <ul>
+                {taskList.map(element => 
+                <li className='listitem' key={Math.floor(Math.random()*1000)} > name: {element.value}, task: {element.value2}, date: {element.value3}, start: {element.value4}, end: {element.value5} <button onClick={deleteTask} className='delete'>X </button> <button className='completed'>V </button></li>
+                )}
+            </ul>
+            : null
+            }
         </div>
     )
 }
